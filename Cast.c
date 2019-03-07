92159c4d10b630a86f265791fbeaba96d806f99b
CrescenzoDeLuca <lmcdfa@gmail.com>
2019-03-07 12:49:15 +0100

    fatt

---
+++ Cast.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{ 
+ double num;
+
+ scanf("%lf", &num);
+ int Conv = (int)num;
+
+  printf("il numero inserito e %f \n ", num);
+
+  printf("in intero sara == %d \n ", Conv);
+}
