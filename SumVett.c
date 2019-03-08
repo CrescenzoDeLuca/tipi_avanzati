6cd6f2a89fe042341b518a1a037524ac0d32f109
CrescenzoDeLuca <lmcdfa@gmail.com>
2019-03-08 23:59:20 +0100

    SumVett

---
+++ SumVett.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+int i, somma, vet[3];
+double media;
+
+printf("inserire i numeri == \n");
+for ( i = 0; i < sizeof(vet) / sizeof(int) ; i++) {
+  scanf("%d\n", &vet[i]);
+}
+
+somma = vet[0] + vet[1] + vet[2];
+media = somma / 3;
+
+printf("Valori iniziali = \n");
+printf("primo = %d \n",vet[0]);
+printf("primo = %d \n",vet[1]);
+printf("primo = %d \n",vet[2]);
+printf("\n");
+
+printf("La somma e' == %d\n", somma );
+
+printf("La media e' == %f\n", media );
+  return 0;
+}
